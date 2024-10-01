a9648c43bcfde9df5d4d14957753af21e410a653
esturm18 <emma.sturm99@gmail.com>
2024-09-30 18:27:37 -0700

    PA1
    PA1 Emma Sturm

---
+++ Module1/WorkflowDemo/Program.cs
@@ -0,0 +1,19 @@
+﻿
+
+/* Name: Emma Sturm
+ * Date: 09/30/2024
+ * CSE 135
+ * PA1: Create a simple program
+ */
+
+namespace WorkflowDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("I am excited for this class");
+        }
+    }
+}
